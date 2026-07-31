aebae8dc1c71fb470fa3cb548bf111e26db9cbaf
raphaeltake <raph.takeuchi@gmail.com>
2026-07-30 22:56:16 -0300

    Estilo tabelas

--- por-do-sol-admin-web/src/app/core/shared/ui/table/table.ts
+++ por-do-sol-admin-web/src/app/core/shared/ui/table/table.ts
@@ -27,6 +27,21 @@ export class Table implements AfterViewInit {
     this.dataSource.paginator = this.paginator;
   }
 
+  // Pega as iniciais do usuário logado para colocar na sidebar
+  getNameInitials(name: string) : string{
+    const firtInitial = name.charAt(0) || "!"
+    let secondInitial = ""
+
+    // Se o usuário ter 2 nomes cadastrados ele pega as duas iniciais
+    if (name.split(" ").length >= 2) {
+
+      // Filtra o nome do usuário para remover nomes como: "da/de". Exemplo: José da Silva, transforma em: José Silva. Iniciais: JS
+      const splitName = name.split(" ").filter(x => x.length > 2)
+      secondInitial = splitName[1].charAt(0)
+    }
+    return `${firtInitial}${secondInitial}`
+  }
+
 }
 
 
