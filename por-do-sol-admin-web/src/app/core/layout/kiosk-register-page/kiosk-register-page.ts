0ed3142a29f415429985358ab42952f8416a5ab4
raphaeltake <raph.takeuchi@gmail.com>
2026-08-05 01:34:21 -0300

    Card preview do quiosque no cadastro

--- por-do-sol-admin-web/src/app/core/layout/kiosk-register-page/kiosk-register-page.ts
+++ por-do-sol-admin-web/src/app/core/layout/kiosk-register-page/kiosk-register-page.ts
@@ -3,10 +3,11 @@ import { KioskRegisterForm } from "../../feature/kiosk-register-form/kiosk-regis
 import { SectionTitle } from "../../shared/ui/section-title/section-title";
 import { Router } from '@angular/router';
 import { ReturnLink } from "../../shared/ui/return-link/return-link";
+import { CardPreview } from "../../shared/card-preview/card-preview";
 
 @Component({
   selector: 'app-kiosk-register-page',
-  imports: [KioskRegisterForm, SectionTitle, ReturnLink],
+  imports: [KioskRegisterForm, SectionTitle, ReturnLink, CardPreview],
   templateUrl: './kiosk-register-page.html',
   styleUrl: './kiosk-register-page.css',
 })
---
+++ por-do-sol-admin-web/src/app/core/shared/card-preview/card-preview.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-preview',
+  imports: [],
+  templateUrl: './card-preview.html',
+  styleUrl: './card-preview.css',
+})
+export class CardPreview {}
